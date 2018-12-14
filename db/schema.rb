1eff5f7a7f00a530ab18604b2c6986a6a23a8b69
ZK <zdk9@yahoo.com>
2018-12-14 14:26:01 -0600

    postgres version

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20181214153737) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "groups", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
@@ -21,7 +24,17 @@ ActiveRecord::Schema.define(version: 20181214153737) do
     t.datetime "updated_at",  null: false
   end
 
-  add_index "groups", ["user_id"], name: "index_groups_on_user_id"
+  add_index "groups", ["user_id"], name: "index_groups_on_user_id", using: :btree
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "secret_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["secret_id"], name: "index_likes_on_secret_id", using: :btree
+  add_index "likes", ["user_id"], name: "index_likes_on_user_id", using: :btree
 
   create_table "members", force: :cascade do |t|
     t.integer  "user_id"
@@ -30,8 +43,17 @@ ActiveRecord::Schema.define(version: 20181214153737) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "members", ["group_id"], name: "index_members_on_group_id"
-  add_index "members", ["user_id"], name: "index_members_on_user_id"
+  add_index "members", ["group_id"], name: "index_members_on_group_id", using: :btree
+  add_index "members", ["user_id"], name: "index_members_on_user_id", using: :btree
+
+  create_table "secrets", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "secrets", ["user_id"], name: "index_secrets_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
@@ -42,4 +64,5 @@ ActiveRecord::Schema.define(version: 20181214153737) do
     t.datetime "updated_at",      null: false
   end
 
+  add_foreign_key "likes", "secrets"
 end
